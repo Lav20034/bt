7efbe8663fefe784b3f5354e46a5416cb418d121
Lav <lavprajapati2003@gmail.com>
2025-03-06 09:06:28 +0530

    dsa-question

---
+++ q1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int n;
+    cout << "Enter your score: ";
+    cin >> n;
+
+    //  ternary operator
+    string result = (n >= 90) ? "A-" :
+                   (n >= 80) ? "B" :
+                   (n >= 70) ? "C" :
+                   (n >= 60) ? "D" : "F";
+
+   
+    cout << result << endl;
+
+   
+  //switch-case
+    
+    switch (n) {
+        case 'A':
+            cout << 'Excellent Work';
+            break;
+        case 'B':
+            cout << 'Well done!';
+            break;
+        case 'C':
+            cout << 'Good job!';
+            break;
+        case 'D':
+            cout << 'You passed, but you could do better';
+            break;
+        case 'F':
+            cout << 'Sorry, you failed';
+            break;
+    }
+
+    //eligibility check
+
+    if ( n= 'F') {
+        cout << 'Congratulations! You are eligible for the next level';
+    } else {
+        cout << " Please try again next time.";
+    }
+
+    cout << endl;
+    return 0;
+}
